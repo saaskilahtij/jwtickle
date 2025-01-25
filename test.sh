0195e94d29399b3d47d54aae9211b971271879f2
johan <saaskilahtijohan@gmail.com>
2025-01-25 19:35:24 +0200

    Init + tests + some functionality

--- test.sh
+++
@@ -1,6 +0,0 @@
-
-echo "With file:" &&
-python3 source/jwtickle.py -f tests/example_secret_b64.token -n &&
-echo "With single token:" &&
-python3 source/jwtickle.py -t "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" -n
-
